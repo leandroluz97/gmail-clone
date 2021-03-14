8667607349f8ef368fcf30a026eeca5910a137e1
Leandro Luz <leandrodaluz97@gmail.com>
2021-03-14 01:20:56 +0000

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,9 @@ import { Button } from "@material-ui/core"
 import React from "react"
 import "./Sidebar.css"
 import AddIcon from "@material-ui/icons/Add"
+import InboxIcon from "@material-ui/icons/Inbox"
+import SidebarOptions from "./SidebarOptions"
+import StarIcon from "@material-ui/icons/Star"
 
 const Sidebar = () => {
   return (
@@ -12,6 +15,14 @@ const Sidebar = () => {
       >
         COMPOSE
       </Button>
+
+      <SidebarOptions
+        Icon={InboxIcon}
+        title='Inbox'
+        number={54}
+        selected={true}
+      />
+      <SidebarOptions Icon={StarIcon} title='Starred' number={17} />
     </div>
   )
 }
